5f0145c3f4214b2787535df87b2c91c415e5ca17
dre258j <dre258j@gmail.com>
2025-04-11 07:35:35 -0500

    Update shorten.js

--- pages/api/shorten.js
+++ pages/api/shorten.js
@@ -1,22 +1,24 @@
-// pages/[id].js
-export async function getServerSideProps({ params, res }) {
-  const id = params.id;
-  const urlDatabase = global.urlDatabase || {};
+let urlDatabase = {};
 
-  const data = urlDatabase[id];
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const { url, custom } = req.body;
 
-  if (data) {
-    data.clicks += 1;
-    res.writeHead(302, { Location: data.originalUrl });
-    res.end();
-    return { props: {} };
+    if (!url) {
+      return res.status(400).json({ error: 'URL is required' });
+    }
+
+    const shortId = custom || Date.now().toString(36);
+
+    if (urlDatabase[shortId]) {
+      return res.status(409).json({ error: 'Alias already in use!' });
+    }
+
+    urlDatabase[shortId] = { originalUrl: url, clicks: 0 };
+
+    const shortenedUrl = `${req.headers.origin}/${shortId}`;
+    res.status(200).json({ shortened_url: shortenedUrl, alias: shortId });
+  } else {
+    res.status(405).end();
   }
-
-  return {
-    notFound: true,
-  };
-}
-
-export default function RedirectPage() {
-  return null;
 }
